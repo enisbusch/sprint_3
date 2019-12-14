f64abe3d0036106c854aa426cbd56cf11fdd92d1
enisbusch <56160782+enisbusch@users.noreply.github.com>
2019-12-14 13:15:14 +0100

    ,

---
+++ Sprint 2/Class2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFITF;
+using MECMOD;
+using PARTITF;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sprint_2
+{
+    class CatiaConnection
+    {
+        INFITF.Application hsp_catiaApp;
+        MECMOD.PartDocument hsp_catiaPart;
+        MECMOD.Sketch hsp_catiaProfil;
+
+        public bool CATIALaeuft()
+        {
+            try
+            {
+                object catiaObject = System.Runtime.InteropServices.Marshal.GetActiveObject(
+                    "CATIA.Application");
+                hsp_catiaApp = (INFITF.Application)catiaObject;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+            public Boolean ErzeugePart()
+            {
+                INFITF.Documents catDocuments1 = hsp_catiaApp.Documents;
+                hsp_catiaPart = catDocuments1.Add("Part") as MECMOD.PartDocument;
+                return true;
+            }
+        public void ErstelleLeereSkizze()
+        {
+            // geometrisches Set auswaehlen und umbenennen
+            HybridBodies catHybridBodies1 = hsp_catiaPart.Part.HybridBodies;
+            HybridBody catHybridBody1;
+            try
+            {
+                catHybridBody1 = catHybridBodies1.Item("Geometrisches Set.1");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Kein geometrisches Set gefunden! " + Environment.NewLine +
+                    "Ein PART manuell erzeugen und ein darauf achten, dass 'Geometisches Set' aktiviert ist.",
+                    "Fehler", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            catHybridBody1.set_Name("Profile");
+            // neue Skizze im ausgewaehlten geometrischen Set anlegen
+            Sketches catSketches1 = catHybridBody1.HybridSketches;
+            OriginElements catOriginElements = hsp_catiaPart.Part.OriginElements;
+            Reference catReference1 = (Reference)catOriginElements.PlaneYZ;
+            hsp_catiaProfil = catSketches1.Add(catReference1);
+
+            // Achsensystem in Skizze erstellen 
+            ErzeugeAchsensystem();
+
+            // Part aktualisieren
+            hsp_catiaPart.Part.Update();
+        }
+        private void ErzeugeAchsensystem()
+        {
+            object[] arr = new object[] {0.0, 0.0, 0.0,
+                                         0.0, 1.0, 0.0,
+                                         0.0, 0.0, 1.0 };
+            hsp_catiaProfil.SetAbsoluteAxisData(arr);
+        }
+        public void ErzeugeProfil(object sender, RoutedEventArgs e)
+        {
+            // Skizze umbenennen
+            hsp_catiaProfil.set_Name("Rechteck");
+
+            // Rechteck in Skizze einzeichnen
+            // Skizze oeffnen
+            Factory2D catFactory2D1 = hsp_catiaProfil.OpenEdition();
+            double hoeheRechteck = Convert.ToDouble(
+            // Rechteck erzeugen
+            Point2D catPoint2D1 = catFactory2D1.CreatePoint();
+            Line2D catLine2D1 = catFactory2D1.CreateLine();
+        }
+        }
+    }
